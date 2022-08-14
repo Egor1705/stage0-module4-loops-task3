c57f2954f9388d6449231ef4cf18fbe44cd34178
Egor1705 <BsuHelp@tut.by>
2022-08-14 21:47:36 +0300

    sum+divisor

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,10 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-int sum = 9;
-        for(int i = 0; i<=lengthOfLastNumber;i++){
-
+int sum = 0;
+if (lengthOfLastNumber == 0){
+    sum = 0;
+}
+int temp = 9;
+        for(int i = 1; i<=lengthOfLastNumber;i++){
+            sum =sum+temp;
+            temp = temp*10+9;
         }
-
+        System.out.println(sum);
     }
 }
